ec4e9ef3bea9e4a960891dbc77ba45d69e1c862d
GY.Xie <GY.Xie@ebensz.com>
2017-09-03 01:54:46 +0800

    完成后端用户注册流程，使用mybatis generate--xgy

--- src/main/java/com/xgy/myweb/service/impl/UserServiceImpl.java
+++ src/main/java/com/xgy/myweb/service/impl/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.xgy.myweb.service.impl;
 
-import com.xgy.myweb.dao.IUserDao;
+import com.xgy.myweb.dao.UserMapper;
+import com.xgy.myweb.model.User;
 import com.xgy.myweb.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -11,7 +12,27 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserServiceImpl implements IUserService {
 
-    private IUserDao userDao;
+    @Autowired
+    private UserMapper userMapper;
 
+    public boolean createUser(User user) {
 
+        if (1 != userMapper.insert(user)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public User getUser(String username) {
+        return null;
+    }
+
+    public boolean deleteUser(String username) {
+        return false;
+    }
+
+    public boolean updateUser(User user) {
+        return false;
+    }
 }
